488fb097fab3fe3e66ece7e4efe39e3fa38983a5
Yurij Alt <yura_chekalyuk@mail.ru>
2021-08-05 17:12:09 +0300

    Initial Commit

---
+++ The Cat App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  The Cat App
+//
+//  Created by Юрий Чекалюк on 05.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
