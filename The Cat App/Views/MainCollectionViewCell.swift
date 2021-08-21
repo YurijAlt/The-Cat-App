eb0a48e9a4dc46ce0fd33a531c01617db4da2638
Yurij Alt <yura_chekalyuk@mail.ru>
2021-08-21 22:17:59 +0300

    Разблокировка основного потока путем добавления DispatchQueue.global(qos: .userInteractive).async {}

--- The Cat App/Views/MainCollectionViewCell.swift
+++ The Cat App/Views/MainCollectionViewCell.swift
@@ -14,10 +14,12 @@ class MainCollectionViewCell: UICollectionViewCell {
     let offlineImageURL = "https://softikbox.com/uploads/images/c0/80/59/c080593d-4efc-4fdf-b646-10c496a5ea28_880x400_fit.png"
     
     func configure(with cat: Cat) {
-        guard let url = URL(string: cat.url ?? offlineImageURL) else { return }
-        guard let imageData = try? Data(contentsOf: url) else { return }
-        DispatchQueue.main.async {
-            self.catImage.image = UIImage(data: imageData)
+        DispatchQueue.global(qos: .userInteractive).async {
+            guard let url = URL(string: cat.url ?? self.offlineImageURL) else { return }
+            guard let imageData = try? Data(contentsOf: url) else { return }
+            DispatchQueue.main.async {
+                self.catImage.image = UIImage(data: imageData)
+            }
         }
     }
     
